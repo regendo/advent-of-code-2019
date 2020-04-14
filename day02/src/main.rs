01f69e7dd4ca59b02e7f57e58e02b82e1e5392c0
Stef B <regendo@users.noreply.github.com>
2020-04-14 22:57:19 +0200

    Day02: Importing seems hard, so here's caching

---
+++ day02/src/main.rs
@@ -0,0 +1,13 @@
+// TODO: re-use day01 as a module instead of using this cached value
+fn base_fuel_for_modules() -> i32 {
+	3_386_686
+}
+
+// TODO: import function from day01 instead
+fn calc_fuel(mass: i32) -> i32 {
+	mass / 3 - 2
+}
+
+fn main() {
+	println!("Hello, world!");
+}
