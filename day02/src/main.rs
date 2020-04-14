d2feffff084266a8d04e2cc98f275b1c8e1066cd
Stef B <regendo@users.noreply.github.com>
2020-04-14 23:18:36 +0200

    Can't test because doc-tests don't run on binaries

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -8,6 +8,31 @@ fn calc_fuel(mass: i32) -> i32 {
 	mass / 3 - 2
 }
 
+/// Total amount of fuel necessary to lift the spaceship. This includes fuel to lift the fuel.
+///
+/// ## Spec
+///
+/// ```
+/// assert_eq!(calc_total_fuel(14), 2);
+/// assert_eq!(calc_total_fuel(1969), 966);
+/// assert_eq!(calc_total_fuel(100_756), 50_346);
+/// ```
+///
+fn calc_total_fuel(base_fuel: i32) -> i32 {
+	let mut total_fuel = 0;
+	let mut additional_fuel = base_fuel;
+
+	while additional_fuel > 0 {
+		total_fuel += additional_fuel;
+		additional_fuel = calc_fuel(additional_fuel);
+	}
+
+	total_fuel
+}
+
 fn main() {
-	println!("Hello, world!");
+	println!(
+		"Actual total fuel required, including fuel for the fuel: {} liters",
+		calc_total_fuel(base_fuel_for_modules())
+	);
 }
