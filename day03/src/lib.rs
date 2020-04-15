efc51758f76e2d8fa1f03f6064838eac07820e65
Stef B <regendo@users.noreply.github.com>
2020-04-16 00:05:43 +0200

    Add Directions and Points

--- day03/src/lib.rs
+++ day03/src/lib.rs
@@ -1,7 +1,42 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+enum Direction {
+	Left(u32),
+	Right(u32),
+	Up(u32),
+	Down(u32),
+}
+
+struct Point {
+	x: i32,
+	y: i32,
+}
+
+impl Point {
+	/// ["Manhattan Distance"](https://en.wikipedia.org/wiki/Taxicab_geometry) between two points.
+	///
+	/// Note that this distance is in absolute values and symmetrical.
+	fn distance_to(&self, other: &Point) -> i32 {
+		(self.x - other.x).abs() + (self.y - other.y).abs()
+	}
+
+	/// Travel a distance from this point, returning all points traveled through.
+	fn travel(&self, direction: Direction) -> Vec<Point> {
+		let distance = match direction {
+			Direction::Left(d) | Direction::Right(d) | Direction::Up(d) | Direction::Down(d) => d,
+		};
+		let mut traveled = Vec::with_capacity(distance as usize);
+
+		for i in 1..=(distance as i32) {
+			#[rustfmt::skip]
+			let bus_stop = match direction {
+				Direction::Left(_)  => Point { x: self.x - i, y: self.y },
+				Direction::Right(_) => Point { x: self.x + i, y: self.y },
+				Direction::Up(_)    => Point { x: self.x,     y: self.y + i },
+				Direction::Down(_)  => Point { x: self.x,     y: self.y - i },
+			};
+
+			traveled.push(bus_stop);
+		}
+
+		traveled
+	}
 }
