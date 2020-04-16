68ec2bb5f0035b9c3bdc9b489e0088e361bf8039
Stef B <regendo@users.noreply.github.com>
2020-04-17 01:58:35 +0200

    Rust 💔 Graphs 😢
    I've heard that graphs don't work well with Rust's borrow rules but I guess I actually gotta read up on that now.

--- day06/src/lib.rs
+++ day06/src/lib.rs
@@ -1,7 +1,45 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use std::collections::HashMap;
+use std::fs;
+use std::rc::Rc;
+
+#[derive(Debug)]
+struct Satellite {
+	name: String,
+	orbit: Option<Rc<Satellite>>,
+}
+
+impl Satellite {
+	fn new(name: &str) -> Rc<Satellite> {
+		Rc::new(Satellite {
+			name: name.to_string(),
+			orbit: None,
+		})
+	}
+}
+
+fn read_input(path: &str) -> Vec<(String, String)> {
+	fs::read_to_string(path)
+		.unwrap()
+		.lines()
+		.map(|l| {
+			let mut split = l.split(')');
+			(
+				split.next().unwrap().to_string(),
+				split.next().unwrap().to_string(),
+			)
+		})
+		.collect()
+}
+
+fn build_solar_system(input: &[(&str, &str)]) {
+	let mut solar_system = HashMap::<&str, Rc<Satellite>>::new();
+
+	for (orb, sat) in input {
+		if !solar_system.contains_key(orb) {
+			solar_system.insert(orb, Satellite::new(orb));
+		}
+		let satellite = solar_system.entry(sat).or_insert(Satellite::new(sat));
+
+		satellite.orbit = Some(Rc::clone(solar_system.get(orb).unwrap()));
+	}
 }
