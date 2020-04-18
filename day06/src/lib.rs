c1987940540f71931522b97630f9324fbd16b93d
Stef B <regendo@users.noreply.github.com>
2020-04-18 20:57:16 +0200

    Calculate orbital transfers

--- day06/src/lib.rs
+++ day06/src/lib.rs
@@ -4,7 +4,7 @@ use std::fs;
 use std::hash::BuildHasher;
 use std::rc::Rc;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct Satellite {
 	name: String,
 	orbit: RefCell<Option<Rc<Satellite>>>,
@@ -22,6 +22,48 @@ impl Satellite {
 		match &*self.orbit.borrow() {
 			None => 0,
 			Some(other) => 1 + other.count_orbits(),
+		}
+	}
+
+	/// Compute the distance between one satellite that (indirectly) orbits another.
+	fn orbit_distance(&self, target: Rc<Satellite>) -> Option<u32> {
+		match &*self.orbit.borrow() {
+			None => None,
+			Some(other) if *other == target => Some(1),
+			Some(other) => match other.orbit_distance(target) {
+				None => None,
+				Some(num) => Some(num + 1),
+			},
+		}
+	}
+
+	/// Find the closest orbit that two satellites share.
+	///
+	/// The function name was chosen for similarity with the greatest common denominator.
+	fn greatest_common_orbit(&self, target: Rc<Satellite>) -> Option<Rc<Satellite>> {
+		if self.orbit.borrow().is_none() {
+			return None;
+		}
+
+		// So this recursive implementation works but if I try to do it iteratively, I can't reassign my variable because it's still borrowed
+		if let Some(sat) = &*target.orbit.borrow() {
+			if self.orbit_distance(Rc::clone(sat)).is_some() {
+				return Some(Rc::clone(sat));
+			} else {
+				return self.greatest_common_orbit(Rc::clone(sat));
+			}
+		}
+
+		None
+	}
+
+	pub fn transfers_between_orbits(&self, other: Rc<Satellite>) -> Option<u32> {
+		if let Some(common_orbit) = self.greatest_common_orbit(Rc::clone(&other)) {
+			let dist_a = self.orbit_distance(Rc::clone(&common_orbit));
+			let dist_b = other.orbit_distance(common_orbit);
+			dist_a.and_then(|a| dist_b.map(|b| a + b))
+		} else {
+			None
 		}
 	}
 }
--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,4 +1,5 @@
 use day06::{build_solar_system, count_total_orbits, read_input};
+use std::rc::Rc;
 
 fn main() {
 	let pairs = read_input("input.txt");
@@ -6,4 +7,10 @@ fn main() {
 	let orbits = count_total_orbits(&solar_system);
 
 	println!("This map data contains a total of {} orbits.", orbits);
+
+	if let (Some(you), Some(san)) = (solar_system.get("YOU"), solar_system.get("SAN")) {
+		if let Some(distance) = (*you).transfers_between_orbits(Rc::clone(san)) {
+			println!("{} orbital transfers required to reach Santa!", distance);
+		}
+	}
 }
