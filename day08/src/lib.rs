fa264faba50e3c7ea05f69bdddfb4e85d823b28c
Stef B <regendo@users.noreply.github.com>
2020-04-19 23:45:51 +0200

    Split into layers, rows

--- day08/src/lib.rs
+++ day08/src/lib.rs
@@ -1,11 +1,56 @@
 use std::fs;
 
-fn read_input(path: &str) -> Vec<u8> {
-	fs::read_to_string(path)
-		.unwrap()
+fn string_to_data(string: &str) -> Vec<u8> {
+	string
 		.trim()
 		.split("")
 		.filter(|s| !s.is_empty())
 		.map(|s| s.parse::<u8>().unwrap())
 		.collect()
+}
+
+fn read_input(path: &str) -> Vec<u8> {
+	string_to_data(&fs::read_to_string(path).unwrap())
+}
+
+fn split_into_layers_by_dimension(data: &[u8], width: usize, height: usize) -> Vec<Vec<&[u8]>> {
+	let layers = data.len() / (width * height);
+	let mut image = Vec::new();
+
+	for layer in 0..layers {
+		let off = layer * width * height;
+		let mut image_layer = Vec::new();
+
+		for row in 0..height {
+			let start = off + row * width;
+			let end = off + (row + 1) * width;
+			image_layer.push(&data[start..end]);
+		}
+
+		image.push(image_layer);
+	}
+
+	image
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn it_splits_the_sample_correctly() {
+		let data = string_to_data("123456789012");
+		let image = split_into_layers_by_dimension(&data, 3, 2);
+		assert_eq!(image.len(), 2);
+		for layer in image.iter() {
+			assert_eq!(layer.len(), 2);
+			for row in layer {
+				assert_eq!(row.len(), 3);
+			}
+		}
+		assert_eq!(image[0][0], &data[0..3]);
+		assert_eq!(image[0][1], &data[3..6]);
+		assert_eq!(image[1][0], &data[6..9]);
+		assert_eq!(image[1][1], &data[9..12]);
+	}
 }
