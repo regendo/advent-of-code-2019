b2953cbb0278a18d79fce36460950f8b5273b835
Stef B <regendo@users.noreply.github.com>
2020-06-18 19:04:23 +0200

    Compare two 2D vectors

--- day10/src/lib.rs
+++ day10/src/lib.rs
@@ -1,11 +1,29 @@
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
 pub struct Point2D(pub usize, pub usize);
 
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
+
+pub struct Vec2D(pub i32, pub i32);
+
 impl Point2D {
+	/// Compute distance (2D vector) between two points.
+	/// # Example
+	/// ```
+	/// # use day10::{Point2D, Vec2D};
+	/// let a = Point2D(4, 4);
+	/// let b = Point2D(2, 5);
+	/// assert_eq!(a.distance_to(&b), Vec2D(-2, 1));
+	/// ```
+	pub fn distance_to(&self, other: Point2D) -> Vec2D {
+		Vec2D(
+			other.0 as i32 - self.0 as i32,
+			other.1 as i32 - self.1 as i32,
+		)
+	}
+
 	/// Return only the points that are visible from this point in 2D space.
 	/// That is, they are not the same as this point and they are not behind another point that is blocking a direct line of sight.
-	// pub fn filter_visible(&self, others: &[Point2D]) -> &[Point2D] {
-	// }
+	// pub fn filter_visible(&self, others: &[Point2D]) -> &[Point2D] {}
 
 	/// Create a number of points from a 2D map where each `#` represents a point and the top left corner is the (0,0) coordinate.
 	/// # Example
@@ -39,6 +57,61 @@ impl Point2D {
 			})
 			.flatten()
 			.collect()
+	}
+}
+
+impl Vec2D {
+	/// Check if this vector is the same as another vector but stretched out.
+	/// # Examples
+	/// ```
+	/// # use day10::Vec2D;
+	/// assert_eq!(true,  Vec2D(2, 3).is_multiple_of(Vec2D(2, 3)));
+	/// ```
+	/// ```
+	/// # use day10::Vec2D;
+	/// assert_eq!(true,  Vec2D(4, 6).is_multiple_of(Vec2D(2, 3)));
+	/// ```
+	/// ```
+	/// # use day10::Vec2D;
+	/// assert_eq!(true,  Vec2D(3, 0).is_multiple_of(Vec2D(2, 0)));
+	/// ```
+	/// ```
+	/// # use day10::Vec2D;
+	/// assert_eq!(false, Vec2D(3, 1).is_multiple_of(Vec2D(2, 0)));
+	/// ```
+	/// ```
+	/// # use day10::Vec2D;
+	/// assert_eq!(false, Vec2D(4, 7).is_multiple_of(Vec2D(2, 3)));
+	/// ```
+	/// ```
+	/// # use day10::Vec2D;
+	/// assert_eq!(false, Vec2D(-4, 0).is_multiple_of(Vec2D(2, 0)));
+	/// ```
+	/// ```
+	/// # use day10::Vec2D;
+	/// assert_eq!(true,  Vec2D(-4, 0).is_multiple_of(Vec2D(-2, 0)));
+	/// ```
+	/// ```
+	/// # use day10::Vec2D;
+	/// assert_eq!(false, Vec2D(-4, -2).is_multiple_of(Vec2D(2, 1)));
+	/// ```
+	pub fn is_multiple_of(self, other: Vec2D) -> bool {
+		if self == other {
+			return true;
+		}
+		match (self, other) {
+			(Vec2D(0, a), Vec2D(0, b)) => a.abs() > b.abs() && a.signum() == b.signum(),
+			(Vec2D(a, 0), Vec2D(b, 0)) => a.abs() > b.abs() && a.signum() == b.signum(),
+			(_, Vec2D(0, _)) => false,
+			(_, Vec2D(_, 0)) => false,
+			(Vec2D(ax, ay), Vec2D(bx, by)) => {
+				let x_factor = ax as f32 / bx as f32;
+				let y_factor = ay as f32 / by as f32;
+
+				// comparing these floats for equality might be an issue
+				x_factor > 1.0 && x_factor == y_factor
+			}
+		}
 	}
 }
 
