27120a68fbbaeaa1225bf98cddea79729dceb792
Stef B <regendo@users.noreply.github.com>
2020-06-18 20:33:01 +0200

    Find optimal location for input (end of part 1)

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use day10::Point2D;
+use std::error::Error;
+use std::fs;
+
+fn main() -> Result<(), Box<dyn Error>> {
+	let input = fs::read_to_string("input.bib")?;
+	let map = Point2D::from_asteroid_map(input.split_whitespace());
+	let station = Point2D::with_highest_visibility(&map).ok_or("No point found")?;
+	println!(
+		"Optimal location for a monitoring station is {:?}, with {} asteroids visible.",
+		station,
+		station.filter_visible(&map).len()
+	);
+
+	Ok(())
 }
