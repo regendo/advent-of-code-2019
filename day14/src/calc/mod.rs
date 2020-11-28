66bb14d5bc889f8e4b50675637fb20c9a5ea57a5
Stef B <regendo@users.noreply.github.com>
2020-11-28 18:31:26 +0100

    Create all the chemicals

--- day14/src/calc/mod.rs
+++ day14/src/calc/mod.rs
@@ -1,11 +1,20 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, error::Error};
 
 use crate::parse::Reaction;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Resources<'a> {
-	required: HashMap<&'a str, u32>,
+	pub required: HashMap<&'a str, u32>,
 	leftover: HashMap<&'a str, u32>,
+}
+
+impl Default for Resources<'static> {
+	fn default() -> Self {
+		Self {
+			required: vec![("FUEL", 1)].into_iter().collect(),
+			leftover: Default::default(),
+		}
+	}
 }
 
 pub fn what_creates<'a, 'b>(haystack: &'a [Reaction<'b>], needle: &str) -> Vec<&'a Reaction<'b>> {
@@ -61,6 +70,40 @@ pub fn react<'a>(reaction: &Reaction<'a>, times: u32, resources: Resources<'a>) 
 	resources
 }
 
+fn is_wanted<'a>(resource: &'a (&&str, &u32)) -> bool {
+	*resource.0 != "ORE" && *resource.1 > 0
+}
+
+pub fn create_next_chemical<'a>(
+	reactions_available: &[Reaction<'a>],
+	resources: Resources<'a>,
+) -> Result<Resources<'a>, Box<dyn Error>> {
+	let (chemical, wanted_amount) = resources
+		.required
+		.iter()
+		.find(is_wanted)
+		.expect("No more chemicals required!");
+	let choices = what_creates(reactions_available, chemical);
+	if choices.is_empty() {
+		return Err(format!("No reaction to create {}!", chemical).into());
+	}
+	// TODO try other options, find the optimal one
+	let reaction = choices.get(0).expect("We checked this earlier.");
+	let amount_per_reaction = reaction.output.1;
+	let times = if wanted_amount % amount_per_reaction > 0 {
+		wanted_amount / amount_per_reaction + 1
+	} else {
+		wanted_amount / amount_per_reaction
+	};
+
+	let resources = react(reaction, times, resources);
+	if resources.required.iter().any(|r| is_wanted(&r)) {
+		create_next_chemical(reactions_available, resources)
+	} else {
+		Ok(resources)
+	}
+}
+
 #[cfg(test)]
 mod tests {
 	use super::*;
@@ -107,5 +150,26 @@ mod tests {
 				.collect(),
 		};
 		assert_eq!(expected, react(&reaction, 5, input_resources));
+	}
+
+	#[test]
+	fn it_creates_the_fuel() {
+		let reactions = crate::parse::parse_reactions(
+			r"9 ORE => 2 A
+8 ORE => 3 B
+7 ORE => 5 C
+3 A, 4 B => 1 AB
+5 B, 7 C => 1 BC
+4 C, 1 A => 1 CA
+2 AB, 3 BC, 4 CA => 1 FUEL",
+		)
+		.unwrap();
+		let input = Resources {
+			required: vec![("FUEL", 1)].into_iter().collect(),
+			leftover: Default::default(),
+		};
+
+		let actual = create_next_chemical(&reactions, input).unwrap();
+		assert_eq!(Some(&165), actual.required.get("ORE"));
 	}
 }
--- day14/src/main.rs
+++ day14/src/main.rs
@@ -4,8 +4,12 @@ use std::{error::Error, fs};
 fn main() -> Result<(), Box<dyn Error>> {
 	let raw_input = fs::read_to_string("input.txt")?;
 	let reactions = parse::parse_reactions(&raw_input)?;
-	let target = ("FUEL", 1);
-	println!("{:?}", calc::what_creates(&reactions, "FUEL"));
+	let resources = calc::create_next_chemical(&reactions, Default::default())?;
+
+	println!(
+		"Used {} ORE to create 1 FUEL.",
+		resources.required.get("ORE").unwrap()
+	);
 
 	Ok(())
 }
